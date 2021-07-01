aa4ae995a0f646555b45f1ace1b12c0344a22bcd
syan <syan@qq.com>
2021-07-01 21:58:53 +0800

    first commit

---
+++ src/main/java/com/syan/git/TestGit.java
@@ -0,0 +1,11 @@
+package com.syan.git;
+
+/**
+ * @author syan
+ */
+public class TestGit {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        System.out.println("hello");
+    }
+}
